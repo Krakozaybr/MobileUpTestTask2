f167a84f0b51d2ce8fcc910a280eeb4b183f6b78
krakozaybr <yaroslaw.gritsencko@mobileup.ru>
2024-09-03 12:29:30 +0300

    Added components to DI

---
+++ features/src/main/kotlin/ru/mobileup/template/features/cryptocurrency/DI.kt
@@ -0,0 +1,51 @@
+package ru.mobileup.template.features.cryptocurrency
+
+import com.arkivanov.decompose.ComponentContext
+import org.koin.core.component.get
+import org.koin.dsl.module
+import ru.mobileup.template.core.ComponentFactory
+import ru.mobileup.template.features.cryptocurrency.domain.CoinId
+import ru.mobileup.template.features.cryptocurrency.presentation.CryptocurrencyComponent
+import ru.mobileup.template.features.cryptocurrency.presentation.RealCryptocurrencyComponent
+import ru.mobileup.template.features.cryptocurrency.presentation.details.CoinDetailsComponent
+import ru.mobileup.template.features.cryptocurrency.presentation.details.RealCoinDetailsComponent
+import ru.mobileup.template.features.cryptocurrency.presentation.list.CoinListComponent
+import ru.mobileup.template.features.cryptocurrency.presentation.list.RealCoinListComponent
+
+val cryptocurrencyModule = module {
+
+}
+
+fun ComponentFactory.createCryptocurrencyComponent(
+    componentContext: ComponentContext,
+): CryptocurrencyComponent {
+    return RealCryptocurrencyComponent(
+        componentContext = componentContext,
+        componentFactory = get()
+    )
+}
+
+fun ComponentFactory.createCoinListComponent(
+    componentContext: ComponentContext,
+    onOutput: (CoinListComponent.Output) -> Unit,
+): CoinListComponent {
+    return RealCoinListComponent(
+        componentContext = componentContext,
+        coinRepository = get(),
+        currencyRepository = get(),
+        errorHandler = get(),
+        onOutput = onOutput
+    )
+}
+
+fun ComponentFactory.createCoinDetailsComponent(
+    coinId: CoinId,
+    componentContext: ComponentContext
+): CoinDetailsComponent {
+    return RealCoinDetailsComponent(
+        componentContext = componentContext,
+        coinId = coinId,
+        coinRepository = get(),
+        errorHandler = get()
+    )
+}
