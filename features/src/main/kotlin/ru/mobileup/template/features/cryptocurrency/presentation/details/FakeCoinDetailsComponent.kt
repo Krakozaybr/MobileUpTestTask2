c059e03635688ae118868b2890983529ddfe833a
krakozaybr <yaroslaw.gritsencko@mobileup.ru>
2024-09-04 13:50:34 +0300

    Fixed FakeCoinDetailsComponent.kt

--- features/src/main/kotlin/ru/mobileup/template/features/cryptocurrency/presentation/details/FakeCoinDetailsComponent.kt
+++ features/src/main/kotlin/ru/mobileup/template/features/cryptocurrency/presentation/details/FakeCoinDetailsComponent.kt
@@ -8,15 +8,18 @@ import ru.mobileup.template.features.cryptocurrency.domain.CoinId
 
 class FakeCoinDetailsComponent : CoinDetailsComponent {
     override val coinState = MutableStateFlow(
-        LoadableState(
-            data = CoinDetails(
-                id = CoinId("btc"),
-                imageLink = "https://media.licdn.com/dms/image/v2/C510BAQGyuGalyYxfXQ/company-logo_200_200/company-logo_200_200/0/1631334696178?e=2147483647&v=beta&t=5TmFyg4zbmhC3J_ByYHr6aYCmFD8ZmNcpoRT8RNs2Kw",
-                description = "Some description".trimIndent(),
-                categories = persistentListOf(
-                    "Smart Contract Platform", "Ethereum Ecosystems"
+        CoinDetailsComponent.Model(
+            details = LoadableState(
+                data = CoinDetails(
+                    id = CoinId("btc"),
+                    imageLink = "https://media.licdn.com/dms/image/v2/C510BAQGyuGalyYxfXQ/company-logo_200_200/company-logo_200_200/0/1631334696178?e=2147483647&v=beta&t=5TmFyg4zbmhC3J_ByYHr6aYCmFD8ZmNcpoRT8RNs2Kw",
+                    description = "Some description".trimIndent(),
+                    categories = persistentListOf(
+                        "Smart Contract Platform", "Ethereum Ecosystems"
+                    )
                 )
-            )
+            ),
+            title = "Bitcoin"
         )
     )
 
