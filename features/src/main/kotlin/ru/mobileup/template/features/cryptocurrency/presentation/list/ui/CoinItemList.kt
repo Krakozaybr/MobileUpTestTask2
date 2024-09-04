e622e98d773d67922ca51ee994e0038510fdb04d
krakozaybr <yaroslaw.gritsencko@mobileup.ru>
2024-09-04 14:14:06 +0300

    CoinListUi

---
+++ features/src/main/kotlin/ru/mobileup/template/features/cryptocurrency/presentation/list/ui/CoinItemList.kt
@@ -0,0 +1,86 @@
+package ru.mobileup.template.features.cryptocurrency.presentation.list.ui
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+import ru.mobileup.template.core.theme.AppTheme
+import ru.mobileup.template.features.cryptocurrency.domain.CoinId
+import ru.mobileup.template.features.cryptocurrency.domain.CoinInfo
+import ru.mobileup.template.features.cryptocurrency.domain.Currency
+
+@Composable
+fun CoinItemList(
+    onCoinClick: (CoinInfo) -> Unit,
+    coinList: ImmutableList<CoinInfo>,
+    modifier: Modifier = Modifier,
+    userScrollEnabled: Boolean = true,
+    contentPaddingValues: PaddingValues = PaddingValues(vertical = 20.dp),
+    spacing: Dp = 0.dp
+) {
+
+    val listAlpha by animateFloatAsState(
+        targetValue = if (!userScrollEnabled) 0.7f else 1f,
+        label = "List alpha animation"
+    )
+
+    LazyColumn(
+        modifier = modifier.graphicsLayer { alpha = listAlpha },
+        contentPadding = contentPaddingValues,
+        userScrollEnabled = userScrollEnabled,
+        verticalArrangement = Arrangement.spacedBy(spacing)
+    ) {
+        items(coinList, key = { it.id.value }) {
+            CoinItem(
+                modifier = Modifier
+                    .clickable { onCoinClick(it) }
+                    .padding(
+                        vertical = 12.dp,
+                        horizontal = 20.dp
+                    ),
+                coin = it,
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun CoinItemListPreview() {
+    AppTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+            CoinItemList(
+                userScrollEnabled = true,
+                onCoinClick = {},
+                coinList = List(10) {
+                    CoinInfo(
+                        id = CoinId("id$it"),
+                        priceChangePercentage = Math.pow(-1.0, it.toDouble()).toFloat() * it * 2,
+                        name = "$it name",
+                        imageLink = "https://media.licdn.com/dms/image/v2/C510BAQGyuGalyYxfXQ/company-logo_200_200/company-logo_200_200/0/1631334696178?e=2147483647&v=beta&t=5TmFyg4zbmhC3J_ByYHr6aYCmFD8ZmNcpoRT8RNs2Kw",
+                        price = it * 1000f,
+                        symbol = "${it}SB",
+                        currency = Currency("RUB")
+                    )
+                }.toImmutableList(),
+            )
+        }
+    }
+}
+
---
+++ features/src/main/kotlin/ru/mobileup/template/features/cryptocurrency/presentation/list/ui/CurrencyChip.kt
@@ -0,0 +1,67 @@
+package ru.mobileup.template.features.cryptocurrency.presentation.list.ui
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import ru.mobileup.template.core.theme.custom.CustomTheme
+
+@Composable
+fun CurrencyChip(
+    isSelected: Boolean,
+    name: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(vertical = 12.dp, horizontal = 30.dp),
+    shape: Shape = RoundedCornerShape(20.dp)
+) {
+    val background by animateColorAsState(
+        targetValue = if (isSelected) {
+            CustomTheme.colors.cryptocurrencyColors.main
+        } else {
+            MaterialTheme.colorScheme.onSurface
+        }.copy(alpha = 0.12f),
+        label = "CurrencyChip background color animation"
+    )
+
+    val text by animateColorAsState(
+        targetValue = if (isSelected) {
+            CustomTheme.colors.cryptocurrencyColors.mainVariant
+        } else {
+            MaterialTheme.colorScheme.onSurface
+        },
+        label = "CurrencyChip text color animation"
+    )
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(shape)
+            .drawBehind {
+                drawRect(
+                    color = background
+                )
+            }
+            .clickable(onClick = onClick)
+            .padding(paddingValues)
+    ) {
+        BasicText(
+            text = name,
+            color = { text },
+            style = CustomTheme.typography.cryptocurrencyTypography.chip
+        )
+    }
+
+}
